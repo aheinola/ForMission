a491da6874c3ab1f00e2e7833aa2243ef8f16ec5
Aapo Heinola <gr289368@gradia.fi>
2025-11-03 10:08:21 +0200

    test

---
+++ tehtävä1/frontend/src/App.jsx
@@ -0,0 +1,164 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+// App component, the main component for the quiz application
+function App() {
+  // Variables for useState
+  const [allQuestions, setAllQuestions] = useState([]);
+  const [selectedTopic, setSelectedTopic] = useState(null);
+  const [score, setScore] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [quizStarted, setQuizStarted] = useState(false);
+  const [username, setUsername] = useState("");
+  const [quizStartTime, setQuizStartTime] = useState(null);
+  const [highScores, setHighScores] = useState([]);
+  const [leaderboardTopic, setLeaderboardTopic] = useState(null); // default topic is none
+
+
+  // Function to fetch and show questions based on selected topic
+  const showQuestions = async () => {
+    const response = await fetch('http://localhost:3000/questions');
+    const data = await response.json();
+
+    if (!selectedTopic) {
+      alert("select topic");
+      return;
+    }
+    const filtered = data.filter(question => question.topic === selectedTopic);
+    setAllQuestions(filtered);
+    setCurrentQuestion(0);
+    setScore(0);
+    setQuizStarted(true);
+    setQuizStartTime(Date.now());
+  };
+
+
+  // Function to handle answer clicks, updates score and the current question
+  const handleAnswerClick = (answerId) => {
+    const question = allQuestions[currentQuestion];
+    if (Number(answerId) === Number(question.correct_answer)) {
+      setScore(prev => prev + 1);
+    }
+    if (currentQuestion + 1 < allQuestions.length) {
+      setCurrentQuestion(prev => prev + 1);
+    } else {
+      const finalScore = score + (Number(answerId) === Number(question.correct_answer) ? 1 : 0);
+      alert(`Quiz done. Your score: ${finalScore} / ${allQuestions.length}`);
+    }
+  };
+
+  const postUserData = async (username, highscore, topic, time) => {
+    const response = await fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username, highscore, topic, time }),
+    });
+    const data = await response.json();
+    console.log('User data saved:', data);
+  };
+
+  const handleEndQuiz = async () => {
+    const timeTaken = Number(((Date.now() - quizStartTime) / 1000).toFixed(1));
+    await postUserData(username, score, selectedTopic, timeTaken);
+
+    if (!username) {
+      alert("write username");
+      return;
+    }
+
+    setQuizStarted(false);
+    setAllQuestions([]);
+    setSelectedTopic(null);
+    setScore(0);
+    setCurrentQuestion(0);
+  };
+
+  useEffect(() => {
+    const fetchHighScores = async () => {
+      const response = await fetch('http://localhost:3000/users');
+      const data = await response.json();
+
+      const filtered = data.filter(user => user.topic === leaderboardTopic);
+
+      const sorted = filtered
+        .sort((a, b) => b.highscore - a.highscore || a.time - b.time)
+        .slice(0, 10);
+
+      setHighScores(sorted);
+    };
+
+    fetchHighScores();
+  }, [leaderboardTopic, quizStarted]);
+
+
+  return (
+    <>
+
+      <div>
+        <a target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+
+      <h1>Tietovisa</h1>
+      <hr />
+
+      {!quizStarted && (
+        <div className='card'>
+
+          <div className="leaderboard">
+            <h2>🏆 Top 10 High Scores</h2>
+            {highScores.length === 0 ? (
+              <p>Select a topic to see highscore/No highscore</p>
+            ) : (
+              <ol>
+                {highScores.map((user, index) => (
+                  <li key={index}>
+                    <strong>{user.username}</strong> with <strong>{user.highscore}</strong> pts ({user.topic}, {user.time}s)
+                  </li>
+                ))}
+              </ol>
+            )}
+          </div>
+          <button onClick={() => { setSelectedTopic("FullStack"); setLeaderboardTopic("FullStack"); }}>Full Stack</button>
+          <button onClick={() => { setSelectedTopic("Maths"); setLeaderboardTopic("Maths"); }}>Simple Maths</button>
+          <button onClick={() => { setSelectedTopic("NBA"); setLeaderboardTopic("NBA"); }}>NBA</button>
+
+
+          <div className="card">
+            <button onClick={showQuestions}>Start quiz</button>
+          </div>
+        </div>
+      )}
+
+      {/* Quiz screen */}
+      {quizStarted && allQuestions.length > 0 && (
+        <div>
+          <form onSubmit={(e) => e.preventDefault()}>
+            <input
+              type="text"
+              placeholder="Enter your username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </form>
+
+          <h2>Topic: {selectedTopic}</h2>
+          <h3>Question {currentQuestion + 1}: {allQuestions[currentQuestion].question}</h3>
+
+          {Object.entries(allQuestions[currentQuestion].answers).map(([answerId, answerText]) => (
+            <button key={answerId} onClick={() => handleAnswerClick(answerId)}>
+              {answerText}
+            </button>
+          ))}
+          <p>Score: {score}</p>
+          <button onClick={handleEndQuiz}>End Quiz</button>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
---
+++ tehtävä1/index.js
@@ -0,0 +1,79 @@
+require('dotenv').config();
+const express = require('express');
+const { supabase } = require('./database.js');
+const cors = require('cors');
+const app = express();
+app.use(express.json()); 
+app.use(cors());
+
+// GET quiz questions
+app.get('/questions', async (request, response) => {
+  const { data, error } = await supabase.from('questions').select('*');
+
+  if (error) {
+    console.error('Error fetching questions:', error);
+    return response.status(500).json({ error: 'Database error' });
+  }
+
+  response.json(data);
+});
+
+// GET all users
+app.get('/users', async (request, response) => {
+  const { data, error } = await supabase.from('users').select('*');
+
+  if (error) {
+    console.error('Error fetching user:', error);
+    return response.status(500).json({ error: 'Database error' });
+  }
+
+  response.json(data);
+});
+
+// GET question by id
+app.get('/questions/:question_id', async (request, response) => {
+  console.log('Route hit');
+  const { id } = request.params; 
+  console.log(id);
+
+  const { data, error } = await supabase
+    .from('questions')
+    .select('*')
+    .eq('question_id', id)
+    .single();
+
+  if (error) {
+    if (error.code === 'PGRST116') {
+      return response.status(404).json({ error: 'User not found' });
+    }
+    console.error('Error fetching user:', error);
+    return response.status(500).json({ error: 'Database error' });
+  }
+
+  response.json(data);
+});
+
+// POST create user
+app.post('/users', async (request, response) => {
+  const body = request.body;
+
+  const { data, error } = await supabase
+    .from('users')
+    .insert([{ username: body.username, highscore: body.highscore, topic: body.topic, time: body.time}])
+    .select()
+    .single();
+
+if (error) {
+  console.error('Supabase insert error:', error.message, error.details, error.hint);
+  return response.status(500).json({ error: error.message, details: error.details });
+}
+
+
+  response.status(201).json(data);
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
